c65a4ac9c893cd45b7a1ea6f031cb2aafd8c4004
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-04-21 12:34:31 -0400

    add header and footer with basic styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+      <div>
+        <header>Todo Lost</header>
+        <p>Tasks go here</p>
+      </div>
+      <footer>
+        <div className='App-footerContainer'>
+          <div className='App-footerElement'>
+            <i class="fa-solid fa-list"></i>
+            <span>All</span>
+          </div>
+          <div className='App-footerElement'>
+            <i class="fa-solid fa-unlock"></i>
+            <span>Active</span>
+          </div>
+          <div className='App-footerElement'>
+            <i class="fa-solid fa-clock-rotate-left"></i>
+            <span>History</span>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
